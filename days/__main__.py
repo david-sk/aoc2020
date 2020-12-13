63986a3ec78036c7a33b141fd1567d03de5d7ed4
david-sk <david.desmeurs@slagkryssaren.com>
2020-12-13 12:51:22 +0100

    Repo update to see packages in alphabetic order.

--- days/__main__.py
+++ days/__main__.py
@@ -3,12 +3,13 @@ import sys
 
 
 if __name__ == '__main__':
-    day_number = sys.argv[1] if len(sys.argv) > 1 else None
-    if not day_number or not day_number.isdigit():
+    day_arg = sys.argv[1] if len(sys.argv) > 1 else None
+    if not day_arg or not day_arg.isdigit():
         print('Usage: python3 -m days [day_number]')
     else:
-        module = import_module(f'days.day{day_number}.day{day_number}')
-        assert hasattr(module, 'main'), 'main attribute expected in day modules'
+        day_package_name = 'day' + ('0' + day_arg if len(day_arg) == 1 else day_arg)
+        module = import_module(f'days.{day_package_name}.day{day_arg}')
+        assert hasattr(module, 'main'), '`main` attribute expected in day modules'
         main = getattr(module, 'main')
-        assert callable(main), 'main attribute must be a callable'
+        assert callable(main), '`main` attribute must be a callable'
         main()
