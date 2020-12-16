c47ea567ea97630268002c2a9bfe8aaba9c28039
david-sk <david.desmeurs@slagkryssaren.com>
2020-12-16 12:40:04 +0100

    Code improvements.

--- days/day14/day14.py
+++ days/day14/day14.py
@@ -33,7 +33,7 @@ def day14_part1(program: List[str]) -> int:
 
 def day14_part2(program: List[str]) -> int:
     NUM_BITS = 36
-    mask, mem = 'X' * NUM_BITS, {}
+    mask, mem = '0' * NUM_BITS, {}
     for line in program:
         if line.startswith('mask'):
             mask = line.replace('mask = ', '')[::-1]
@@ -43,20 +43,20 @@ def day14_part2(program: List[str]) -> int:
             )
             str_bin_address = bin(address).replace('0b', '')
             str_bin_address = (('0' * (NUM_BITS - len(str_bin_address))) + str_bin_address)[::-1]
-            x_indexes = [i for i, c in enumerate(mask) if c == 'X']
+            powers = [2 ** i for i, c in enumerate(mask) if c == 'X']
             first_address = sum(
                 2 ** i
                 for i, c in enumerate(mask)
                 if c != 'X' and (c == '1' or str_bin_address[i] == '1')
             )
             mem[first_address] = value
-            for num_indexes in range(1, len(x_indexes) + 1):
-                for combination in combinations(x_indexes, r=num_indexes):
-                    mem[first_address + sum(2 ** x_index for x_index in combination)] = value
+            for num_powers in range(1, len(powers) + 1):
+                for combination in combinations(powers, r=num_powers):
+                    mem[first_address + sum(power for power in combination)] = value
     return sum(mem.values())
 
 
 def main() -> None:
     program = get_program()
-    print('Day13, part1 answer:', day14_part1(program))
-    print('Day13, part2 answer:', day14_part2(program))
+    print('Day14, part1 answer:', day14_part1(program))
+    print('Day14, part2 answer:', day14_part2(program))
